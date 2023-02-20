9b0653060631f02bce3d35ce92bcab20bb3f2e0d
Zachary Robinson <me@zsrobinson.com>
2023-02-19 21:42:10 -0500

    added aria-label for theme toggle

--- src/components/theme-icon.tsx
+++ src/components/theme-icon.tsx
@@ -16,7 +16,10 @@ export default function ThemeIcon() {
   }, [darkMode]);
 
   return (
-    <button onClick={() => setDarkMode((x) => !x)}>
+    <button
+      onClick={() => setDarkMode((x) => !x)}
+      aria-label="Toggle Between Light and Dark Theme"
+    >
       {darkMode ? (
         /* prettier-ignore */
         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-sun" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">

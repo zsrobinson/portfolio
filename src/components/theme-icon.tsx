adb0ee595cf0600e8f2b8cf2edb08ab2399acd8c
Zachary Robinson <me@zsrobinson.com>
2023-06-10 20:16:53 -0400

    use tabler icon import, update bio a bit

--- src/components/theme-icon.tsx
+++ src/components/theme-icon.tsx
@@ -1,3 +1,4 @@
+import { IconMoon, IconSun } from "@tabler/icons-react";
 import { useState } from "react";
 
 export default function ThemeIcon() {
@@ -20,20 +21,7 @@ export default function ThemeIcon() {
 
   return (
     <button onClick={toggle} aria-label="Toggle Between Light and Dark Theme">
-      {darkMode ? (
-        /* prettier-ignore */
-        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-sun" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
-          <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
-        </svg>
-      ) : (
-        /* prettier-ignore */
-        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-moon" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
-        </svg>
-      )}
+      {darkMode ? <IconSun /> : <IconMoon />}
     </button>
   );
 }

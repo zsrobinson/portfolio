1f77033f5e6b53ffbc5d24e59e07a3bfd67d18d0
Zachary Robinson <me@zsrobinson.com>
2023-06-10 21:34:32 -0400

    edit rss feed stuff

--- src/pages/index.xml.ts
+++ src/pages/index.xml.ts
@@ -9,11 +9,16 @@ export const get: APIRoute = async (context) => {
     description:
       "Hey there, my name's Zach! I'm a rising freshman at the University of Maryland, studying Computer Science. I love all things tech, and I'm especially interested in full-stack web development",
     site: context.site?.toString() ?? "",
+    customData: `<atom:link href="${
+      context.site?.toString() ?? ""
+    }index.xml" rel="self" type="application/rss+xml"/>`,
+    xmlns: { atom: "http://www.w3.org/2005/Atom" },
+
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
-      author: "Zachary Robinson",
       link: `/blog/${post.slug}/`,
+      description: post.body.split(" ").slice(0, 50).join(" ") + "...",
     })),
   });
 };

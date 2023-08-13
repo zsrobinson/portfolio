1fc7dae57651fbf266c2ddafbcf9225c751df6e5
Zachary Robinson <me@zsrobinson.com>
2023-08-13 16:44:09 -0400

    change back to /posts/* from /blog/*

--- src/pages/index.xml.ts
+++ src/pages/index.xml.ts
@@ -3,9 +3,9 @@ import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = async (context) => {
-  const blog = await getCollection("blog");
+  const posts = await getCollection("posts");
   return rss({
-    title: "Zachary Robinson's Blog",
+    title: "Zachary Robinson",
     description:
       "Hey there, my name's Zach! I'm a rising freshman at the University of Maryland, studying Computer Science. I love all things tech, and I'm especially interested in full-stack web development",
     site: context.site?.toString() ?? "",
@@ -14,10 +14,10 @@ export const get: APIRoute = async (context) => {
     }index.xml" rel="self" type="application/rss+xml"/>`,
     xmlns: { atom: "http://www.w3.org/2005/Atom" },
 
-    items: blog.map((post) => ({
+    items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
-      link: `/blog/${post.slug}/`,
+      link: `/posts/${post.slug}/`,
       description: post.body.split(" ").slice(0, 50).join(" ") + "...",
     })),
   });

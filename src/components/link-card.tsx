bea943414ba547b81059b8971704c1cf590a3fc4
Zachary Robinson <me@zsrobinson.com>
2023-08-13 21:53:23 -0400

    setup prettier and tailwindcss sorting

--- src/components/link-card.tsx
+++ src/components/link-card.tsx
@@ -6,7 +6,7 @@ export function LinkCard({ children, ...rest }: Props) {
   return (
     <a
       {...rest}
-      className="rounded-lg border border-border text-card-foreground px-4 py-4 bg-accent/70 dark:bg-accent/30 transition-colors shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/50 focus-visible:ring-offset-2 ring-offset-background"
+      className="rounded-lg border border-border bg-accent/70 px-4 py-4 text-card-foreground shadow-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/50 focus-visible:ring-offset-2 dark:bg-accent/30"
     >
       {children}
     </a>

861e4fc55fbfd43022cbef76cbe3057926631346
Zachary Robinson <me@zsrobinson.com>
2023-08-14 21:01:22 -0400

    increase post count, consolidate utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,3 +27,18 @@ export function getSortedTags(posts: CollectionEntry<"posts">[]) {
     return b[1] - a[1];
   });
 }
+
+export function formatDate(date: Date) {
+  return date.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+export function formatMonth(date: Date) {
+  return date.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "long",
+  });
+}

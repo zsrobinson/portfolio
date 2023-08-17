46ca5a81036c47e5f08d6f43f10628ee318cb5b0
Zachary Robinson <me@zsrobinson.com>
2023-08-16 23:37:19 -0400

    revert "try using edge runtime"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/plugins/remarkReadingTime.mjs";
 
@@ -14,7 +14,8 @@ export default defineConfig({
   },
 
   output: "hybrid",
-  adapter: vercel({ imageService: true }),
-  experimental: { assets: true },
+  adapter: vercel(),
   site: "https://zsrobinson.com",
+
+  experimental: { assets: true },
 });

b9624959cd6e3e18b01f67706337ad05365a8cab
Zachary Robinson <me@zsrobinson.com>
2023-10-17 12:26:05 -0400

    format exif details a bit and other stuff

---
+++ src/components/exif-details.tsx
@@ -0,0 +1,74 @@
+import {
+  IconAspectRatio,
+  IconCamera,
+  IconGrain,
+  IconMapPin,
+  IconMountain,
+  IconPhotoSensor,
+  IconRun,
+  type Icon,
+  IconAngle,
+} from "@tabler/icons-react";
+import type Exif from "exif";
+import type { ReactNode } from "react";
+import { aspectRatio, formatGPS } from "~/lib/utils";
+
+export function ExifDetails({ details }: { details: Exif.ExifData }) {
+  return (
+    <div className="flex flex-col gap-1">
+      <Detail Icon={IconCamera} title="Camera">
+        {details.image.Make + " " + details.image.Model}
+      </Detail>
+      <Detail Icon={IconAspectRatio} title="Dimensions">
+        {details.exif.ExifImageWidth} x {details.exif.ExifImageHeight} (
+        {aspectRatio(
+          details.exif.ExifImageWidth!,
+          details.exif.ExifImageHeight!,
+        )}
+        )
+      </Detail>
+      <Detail Icon={IconAngle} title="Focal Length">
+        {details.exif.FocalLengthIn35mmFormat} mm
+      </Detail>
+      <Detail Icon={IconMountain} title="Aperture">
+        f{details.exif.FNumber}
+      </Detail>
+      <Detail Icon={IconRun} title="Shutter">
+        1/{1 / details.exif.ExposureTime!} s
+      </Detail>
+      <Detail Icon={IconGrain} title="ISO">
+        ISO {details.exif.ISO}
+      </Detail>
+      <Detail
+        Icon={IconMapPin}
+        href={"https://www.google.com/maps?q=loc:" + formatGPS(details.gps)}
+        title="GPS"
+      >
+        {formatGPS(details.gps)}
+      </Detail>
+    </div>
+  );
+}
+
+type DetailProps = {
+  Icon: Icon;
+  href?: string;
+  title?: string;
+  children: ReactNode;
+};
+
+function Detail({ Icon, href, title, children }: DetailProps) {
+  return (
+    <div className="flex items-center gap-2" title={title}>
+      <Icon size={16} className="text-muted-foreground" />
+      <span className="w-24 text-sm text-muted-foreground">{title}</span>
+      {href ? (
+        <a href={href} className="font-mono text-sm underline">
+          {children}
+        </a>
+      ) : (
+        <span className="font-mono text-sm">{children}</span>
+      )}
+    </div>
+  );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import type { CollectionEntry } from "astro:content";
 import { clsx, type ClassValue } from "clsx";
+import Exif from "exif";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -41,4 +42,56 @@ export function formatMonth(date: Date) {
     year: "numeric",
     month: "long",
   });
+}
+
+/** Doubt this works more generally but props to trial and error */
+export function getFilePath(path: string) {
+  if (path.includes("_astro")) {
+    return (
+      "src/assets/gallery/" +
+      path.split("/")[2].split(".")[0] +
+      "." +
+      path.split("/")[2].split(".")[2]
+    );
+  } else {
+    return "src/" + path.split("/src/")[1].split("?")[0];
+  }
+}
+
+/** Given an Image object, return the metadata embedded in that image */
+export async function getExif(
+  photo: CollectionEntry<"gallery-posts">["data"]["photos"][0]["path"],
+): Promise<Exif.ExifData> {
+  return new Promise<Exif.ExifData>((resolve, reject) => {
+    new Exif.ExifImage({ image: getFilePath(photo.src) }, (error, exif) => {
+      error ? reject(error) : resolve(exif);
+    });
+  });
+}
+
+/** get info format for google maps URL */
+export function formatGPS(gps: Exif.ExifData["gps"]) {
+  const latitude = gps.GPSLatitude!;
+  const longitude = gps.GPSLongitude!;
+
+  let latitudeDecimal = latitude[0] + latitude[1] / 60 + latitude[2] / 3600;
+  let longitudeDecimal = longitude[0] + longitude[1] / 60 + longitude[2] / 3600;
+
+  if (gps.GPSLongitudeRef! === "W") {
+    longitudeDecimal = -longitudeDecimal;
+  }
+
+  return `${latitudeDecimal.toFixed(6)}, ${longitudeDecimal.toFixed(6)}`;
+}
+
+/** @link https://stackoverflow.com/a/17445304/15938350 */
+function gcd(a: number, b: number) {
+  if (!b) return a;
+  return gcd(b, a % b);
+}
+
+/** Returns a string representation of an aspect ratio */
+export function aspectRatio(width: number, height: number) {
+  const gcdResult = gcd(width, height);
+  return `${width / gcdResult}:${height / gcdResult}`;
 }
